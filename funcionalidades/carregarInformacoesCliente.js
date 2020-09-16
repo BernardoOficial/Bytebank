261aec02482c82d41c69a7c7eaaaff0f4bf78b73
Bernardo <bernardo.258@hotmail.com>
2020-09-16 16:33:21 -0300

    Tela do cliente funcional

--- funcionalidades/carregarInformacoesCliente.js
+++ funcionalidades/carregarInformacoesCliente.js
@@ -7,16 +7,28 @@ function atualizarLogin() {
 
 let boasVindas = document.querySelector('.main__boasVindas');
 
+let infoCliente = document.querySelector('[data-cliente]');
+let infoAgencia = document.querySelector('[data-agencia]');
+let infoSaldo = document.querySelector('[data-saldo]');
+
+console.log(infoAgencia);
+
 function carregaInformacoesCliente() {
 
     atualizarLogin();
 
     if (loginAtual[0]) {
         let nome = loginAtual[0].nome;
+        let agencia = "****";
+        let saldo = "R$ 0.0"
 
         const h1 = document.createElement('h1');
         h1.classList.add('main__boasVindas__titulo');
         h1.textContent = `Boas vindas, ${nome}!`;
+
+        infoCliente.textContent += nome;
+        infoAgencia.textContent += agencia;
+        infoSaldo.textContent += saldo;
 
         boasVindas.appendChild(h1);
     }
